a1ca25a6b0a54f171ed4cc0f19d7954a552a5abe
amanirmk <amainakilaas@hmc.edu>
2020-09-19 00:03:04 -0700

    first day of hackmit !!

---
+++ main.js
@@ -0,0 +1,28 @@
+//aqi =  undefined for now
+//Basically we need to set different template messages depending on what the aqi
+//value is. We would be defining the aqi value as whatever value we retrieve 
+// from the site
+
+if (aqi > 51 && aqi <100){
+    
+} else{
+
+}
+
+if (aqi > 101 && aqi <150){
+    
+} else{
+
+}
+
+if (aqi > 151 && aqi <200){
+    
+} else{
+
+}
+
+if (aqi > 201 && aqi <300){
+    
+} else{
+
+}
