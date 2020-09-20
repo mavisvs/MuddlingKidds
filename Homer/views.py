d8d9fcb8b72a9abfa2af8aacde4557ba6a1f8791
Thaxter Shaw <tshaw@students.lfanet.org>
2020-09-19 23:35:36 -0400

    working aqi

--- Homer/views.py
+++ Homer/views.py
@@ -3,19 +3,33 @@ from Homer import app#, db
 from flask import render_template, request, redirect
 # from Homer.models import Fact
 
+wasteLocation = "amani"
+AQI = "was"
+carbonPPM = "here"
+naturalDisastors = ["list", "here"]
+
 @app.route("/")
 def index():
     return render_template("index.html")
 
 @app.route("/results", methods = ["GET", "POST"])
 def results():
+    global wasteLocation
+    global AQI
+    global carbonPPM
+    global naturalDisastors
     # scrape()
-    print(request.form)
-    AQI = "was"
-    if request.method == "POST":
-        jsdata=request.form["aqi"]
-        AQI = jsdata["data"][0]["aqi"]
-    naturalDisastors = ["list", "here"]
-    wasteLocation = "amani"
-    carbonPPM = "here"
+    
     return render_template("results.html", data={"natDis":naturalDisastors, "waste":wasteLocation, "AQI":AQI, "carbon":carbonPPM})
+
+@app.route("/getData", methods = ["POST"])
+def getData():
+    global wasteLocation
+    global AQI
+    global carbonPPM
+    global naturalDisastors
+    if request.method == "POST":
+        jsdata=request.json()
+        AQI = jsdata["data"][0]["aqi"]
+    return redirect("/results")
+
